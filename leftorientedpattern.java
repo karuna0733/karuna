6f22c3e8d14471288498b0e72db63ef15ef8a9f2
karuna0733 <karunakarana284@gmail.com>
2025-09-09 11:47:33 +0530

    Create leftorientedpattern.java

---
+++ leftorientedpattern.java
@@ -0,0 +1,11 @@
+public class leftorientedpattern {
+    public static void main(String[] args) {
+        int rows = 5; 
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows; j >= i; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
