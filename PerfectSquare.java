1b6c9a3660ebead8f8c37bb80b7aa8cad5a01093
karuna0733 <karunakarana284@gmail.com>
2025-09-18 16:23:05 +0530

    Create PerfectSquare.java

---
+++ PerfectSquare.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sqrt = (int) Math.sqrt(n);
+        if (sqrt * sqrt == n)
+            System.out.println("Perfect Square");
+        else
+            System.out.println("Not Perfect Square");
+    }
+}
