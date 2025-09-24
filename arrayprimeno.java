c428d461187b8112baee39abf6bea83eed8baf04
karuna0733 <karunakarana284@gmail.com>
2025-09-24 09:30:08 +0530

    Create arrayprimeno.java

---
+++ arrayprimeno.java
@@ -0,0 +1,22 @@
+public class arrayprimeno {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        };
+        System.out.println("Prime numbers in the array:");
+        for (int num : arr) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+}
