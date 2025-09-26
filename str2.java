2e4e1a52b4e622a0ce01f0b4b8e264633678c155
karuna0733 <karunakarana284@gmail.com>
2025-09-26 10:30:29 +0530

    Create str2.java

---
+++ str2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class str2 {
+    public static void main(String[] args) {
+   Scanner kbd = new Scanner(System.in);
+   String name = kbd.nextLine();
+   String name1 = kbd.nextLine();
+    System.out.println(name.compareTo(name1));
+    }
+}
