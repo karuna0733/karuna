54005d3becc6f5c8e6dbb8e31e552e171b978f61
karuna0733 <karunakarana284@gmail.com>
2025-09-26 11:40:29 +0530

    Create str3.java

---
+++ str3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class str3 {
+    public static void main(String[] args) {
+   Scanner kbd = new Scanner(System.in);
+   String name = kbd.nextLine();//ASIA CUP
+   System.out.println(name.indexOf("SI"));
+   System.out.println(name.lastIndexOf("SI"));
+    }
+}
+
