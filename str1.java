391bbc55513ac911a0ca553169c6fa8566979884
karuna0733 <karunakarana284@gmail.com>
2025-09-26 10:30:25 +0530

    Create str1.java

---
+++ str1.java
@@ -0,0 +1,8 @@
+public class str1 {
+    public static void main(String[] args) {
+     String name = "Helllo Java World";
+     String name1 = "Helllo Java World";
+     System.out.println ((name).equals(name1));
+    
+    }
+}
