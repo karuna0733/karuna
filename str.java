ce9cf3c8a6889ae3fa2fd052f5626bf006d9bde4
karuna0733 <karunakarana284@gmail.com>
2025-09-26 10:30:21 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,8 @@
+public class str {
+    public static void main(String[] args) {
+     StringBuilder name = new StringBuilder("cse");
+StringBuilder name1 = new StringBuilder("cse");
+System.out.println(name.hashCode());
+System.out.println(name1.hashCode());
+    }
+}
